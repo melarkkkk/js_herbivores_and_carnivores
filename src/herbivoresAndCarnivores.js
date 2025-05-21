903e33f7e1e78841a40ec04ee88ccd1c94467411
Zoriana Melnychuk <oriankaz17@gmail.com>
2025-05-21 19:42:23 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -24,7 +24,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(victim) {
-    if (!victim.hidden && !(victim instanceof Carnivore)) {
+    if (victim instanceof Herbivore && !victim.hidden) {
       victim.health -= 50;
     }
 
